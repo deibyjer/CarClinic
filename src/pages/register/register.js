02b1ec64c91fb174f8ddbae8bf8195f8816853b0
Deibyyy <deibyjer@gmail.com>
2020-11-13 12:46:33 +0000

    Register page

--- src/pages/register/register.js
+++ src/pages/register/register.js
@@ -1,4 +1,5 @@
 ﻿import React from 'react';
+import {Form} from "semantic-ui-react";
 
 class Register extends React.Component {
     render() {
@@ -66,12 +67,12 @@ class PageContent extends React.Component {
 class RegisterForm extends React.Component {
     render() {
         return (
-          <div>
-              <div>
-                  Sign Up!
-              </div>
-              <FormStepOne />
-          </div>  
+            <Form>
+                <FormStepOne />
+                <FormStepTwo />
+                <FormStepThree />
+                <FormStepFour />
+            </Form>
         );
     }
 }
@@ -79,9 +80,11 @@ class RegisterForm extends React.Component {
 class FormStepOne extends React.Component {
     render() {
         return (
-          <div>
-
-          </div>  
+            <Form.Group>
+                <Form.Input label='Email' type='email' />
+                <Form.Input label='Password' type='password' />
+                <Form.Input label='Repeat Password' type='password' />
+            </Form.Group>  
         );
     }
 }
@@ -89,9 +92,11 @@ class FormStepOne extends React.Component {
 class FormStepTwo extends React.Component {
     render() {
         return (
-            <div>
-                Step two
-            </div>
+            <Form.Group>
+                <Form.Select label='I am a' placeholder='Select Service' />
+                <Form.Select label='Registering as' placeholder='Select Type' />
+                <Form.Input label='Company Name/Full Name' type='text' />
+            </Form.Group>
         );
     }
 }
@@ -99,9 +104,14 @@ class FormStepTwo extends React.Component {
 class FormStepThree extends React.Component {
     render() {
         return (
-            <div>
-                Step three
-            </div>
+            <Form.Group>
+                <Form.Input label='HMRC Number (if applicable)' type='text' />
+                <Form.Input label='Address Line 1' type='text' />
+                <Form.Input label='Address Line 2' type='text' />
+                <Form.Input label='Post Code' type='text' />
+                <Form.Input label='City' type='text' />
+                <Form.Input label='Phone Number' type='text' />
+            </Form.Group>
         );
     }
 }
@@ -109,9 +119,13 @@ class FormStepThree extends React.Component {
 class FormStepFour extends React.Component {
     render() {
         return (
-            <div>
-                Step four
-            </div>
+            <Form.Group>
+                <Form.Select label='Primary Services' placeholder='Select Service' />
+                <Form.Select label='Secondary Services' placeholder='Select Service' />
+                <Form.Select label='Car Collection Services' placeholder='Select Service' />
+                <Form.Input label='Brand Specialty' type='text' />
+                <Form.Input label='Tell us about your company/self!' type='text' />
+            </Form.Group>
         );
     }
 }
